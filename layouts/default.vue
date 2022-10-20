d55a715d5baf1a9817b759ee10d6c8c3e77b0884
Robert Nubla <rnubla97@gmail.com>
2022-10-19 22:04:18 -0400

    Project setup with primevue

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <DefaultLayoutTopMenuBar />
+        <div class="flex flex-row card-container ">
+            <div class="flex  flex-none">Sidebar</div>
+            <div class="flex-grow-1 flex">
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <h5>Default Layout</h5>
+        </div>
+        <div class="card-body">
+            <p>Default layout is used for the pages that are not defined in the router.</p>
+        </div>
+    </div>
+
+</template>
